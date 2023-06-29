75b0458ffcdf35af1fc62455b073fa05e275708c
Juan Diego A.H <j.diego93@outlook.com>
2023-06-28 22:32:35 -0600

    TEST FOR RAILWAY 1.03

--- config/db.py
+++ config/db.py
@@ -11,7 +11,7 @@ load_dotenv()
 USER = os.environ.get("USER")
 PASSWORD = os.environ.get("PASSWORD")
 
-uri = "mongodb+srv://" + USER  + ":" + PASSWORD + "@cluster0.muda7ep.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://" + str(USER)  + ":" + str(PASSWORD) + "@cluster0.muda7ep.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client_atlas = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
